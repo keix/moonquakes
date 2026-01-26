574e5a27f5f6a9bf70df300ada5bbb2558500d51
Kei Sawamura <kei@9th.tokyo>
2026-01-27 00:54:58 +0900

    Implement while statement lowering

---
+++ scripts/control_flow/while.lua
@@ -0,0 +1,7 @@
+function test(n)
+    while n > 0 do
+        return n
+    end
+    return 0
+end
+return test(5)
---
+++ scripts/control_flow/while_false.lua
@@ -0,0 +1,7 @@
+function test(n)
+    while n > 10 do
+        return n
+    end
+    return 0
+end
+return test(5)
