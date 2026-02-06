e2d132e85de8a7132c0a9259c9b96fc5cfaf5f20
Kei Sawamura <kei@9th.tokyo>
2026-02-07 02:14:56 +0900

    Pin Zig toolchain via Nix flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,34 @@
 {
+  description = "Zig 0.15.2 Dev Environment";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     zigpkgs.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, zigpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [ zigpkgs.overlays.default ];
-    };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        pkgs.zigpkgs."0.15.2"
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils, zigpkgs }:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux" "aarch64-linux" "aarch64-darwin"
+    ] (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ zigpkgs.overlays.default ];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "zig-0.15.2-shell";
+
+        packages = [
+          pkgs.zigpkgs."0.15.2"
+        ];
+
+        shellHook = ''
+          echo "Zig DevShell loaded (${system})"
+          echo "  zig: $(zig version)"
+        '';
+      };
+    }
+  );
 }
